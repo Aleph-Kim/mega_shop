56c72b67fdc6ce1c08c7bdebde0a117b476db96b
kimchaemin000617 <dktjdej25@gmail.com>
2021-12-27 11:08:23 +0900

    product_list paging 구현

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.contrib.contenttypes.models import ContentType
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -7,8 +8,14 @@ from questions.models import Question
 
 
 def index(request):
+    page = request.GET.get('page', '1')  # GET방식으로 호출 된 url에서 page값을 가져온다.(defult값은 1로 설정한다.)
+
     product_list = Product.objects.all()
-    context = {'product_list': product_list}
+
+    paginator = Paginator(product_list, 5)  # Paginator 클래스 사용, 한 페이지당 5개씩 보여주기
+    page_obj = paginator.get_page(page)  # 요청된 page에 해당되는 객체를 page_obj에 담는다.
+
+    context = {'product_list': page_obj}
     return render(request, 'products/product_list.html', context)
 
 
