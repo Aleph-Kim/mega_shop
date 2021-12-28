0867b92fda320bafa712b2f8eff36fcd49743705
kimchaemin000617 <dktjdej25@gmail.com>
2021-12-28 23:55:35 +0900

    product_list.html 수정(tailwind 적용 및 디자인)

--- products/views.py
+++ products/views.py
@@ -11,9 +11,9 @@ from questions.models import Question
 def index(request):
     page = request.GET.get('page', '1')  # GET방식으로 호출 된 url에서 page값을 가져온다.(defult값은 1로 설정한다.)
 
-    product_list = Product.objects.all()
+    product_list = Product.objects.all().order_by('-id')
 
-    paginator = Paginator(product_list, 5)  # Paginator 클래스 사용, 한 페이지당 5개씩 보여주기
+    paginator = Paginator(product_list, 10)  # Paginator 클래스 사용, 한 페이지당 5개씩 보여주기
     page_obj = paginator.get_page(page)  # 요청된 page에 해당되는 객체를 page_obj에 담는다.
 
     context = {'product_list': page_obj}
